c15213a6a44e2966e7a8ab3a25b57be8224f9832
Sacha FROMENT <sacha@hjkl.it>
2022-01-24 01:22:03 +0100

    particpate button chad

--- src/lib/ParticipateButton.svelte
+++ src/lib/ParticipateButton.svelte
@@ -31,7 +31,7 @@
 		}, duration + 100);
 	}
 </script>
-
+<div class="wrapper" style="height: { button?.clientHeight + 15 || 0 }px">
 <button on:click={click} type="button" bind:this={button}>
 	<section class="flex justify-center">
 		{#if confettiTime}
@@ -44,16 +44,32 @@
 	</section>
 	📊 {$t('index.participants-btn', { participants: Math.ceil($values) })}
 </button>
+</div>
 
 <style lang="postcss">
 	button {
-		@apply max-w-2xl inline-block bg-gradient-to-r from-red-500 to-red-600 hover:from-red-600
+		@apply w-full max-w-2xl inline-block bg-gradient-to-r from-red-500 to-red-600 hover:from-red-600
         hover:to-red-700  focus:from-red-700 focus:to-red-800 transition-all
          text-white px-4 py-4 rounded-3xl
-         border-b-red-900 border-b-8 hover:border-b-4  active:border-0
+         border-b-red-900 border-b-8
         text-2xl font-bold border-opacity-50 shadow-lg
-         outline-none backdrop-blur-lg backdrop-filter  bottom-0 flex-auto m-2;
+         outline-none backdrop-blur-lg backdrop-filter  absolute bottom-0
+		 -translate-x-1/2 left-1/2 z-10;
 	}
+
+	button:hover {
+		border-bottom-width: 10px;
+	}
+	button {
+		border-bottom-width: 16px;
+	}
+	button:active {
+		border-bottom-width: 0px;
+	}
+	.wrapper {
+		@apply flex-auto relative m-2;
+	}
+
 	/*
 	button:hover {
 		padding-bottom: calc(1rem + 4px);
