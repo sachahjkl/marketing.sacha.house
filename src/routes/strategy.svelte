c19f109055054b30f5941dcc54de340e5ad50ffa
Sacha FROMENT <sacha@hjkl.it>
2022-02-07 14:19:43 +0100

    stratégie

--- src/routes/strategy.svelte
+++ src/routes/strategy.svelte
@@ -10,3 +10,54 @@
 </svelte:head>
 
 <h1>stratégie</h1>
+
+<p>Sujet : seconde main </p>
+<p>I- Strat&eacute;gie marketing</p>
+<p>fiche : <a href="https://www.google.com/url?q=http://kmcms.net/Doc/Experiences/secondhand/&amp;sa=D&amp;source=editors&amp;ust=1644243440587416&amp;usg=AOvVaw1G_pwz6rJARPAShA7gEsDk">http://kmcms.net/Doc/Experiences/secondhand/</a>&nbsp;</p>
+<ol start="1">
+<li>
+<h2>R&eacute;alisation marketing pour amener sur la landingpage</h2>
+</li>
+</ol>
+<p>Public cible</p>
+<p>Nous allons cibler les femmes &acirc;g&eacute;es de 18-75 ans qui habitent en France ou en Tunisie.</p>
+<p>R&eacute;alisation</p>
+<ul>
+<li>R&eacute;seaux : Nous comptons avoir un compte instagram, snapshap, Facebook, Twitter, TikTok, Marmiton et (site sp&eacute;cialis&eacute;). (regarder : statista, social blade). Inciter le electronic word of mouth.</li>
+<li>QR code : Afin d'acqu&eacute;rir un maximum de trafic et de nous faire conna&icirc;tre, nous comptons disposer strat&eacute;giquement des QR code. </li>
+</ul>
+<p>Mode op&eacute;ratoire</p>
+<ul>
+<li>Partager/r&eacute;pondre &agrave; des # pr&eacute;cis avec des images promotionnelles pour le site avec adresse du site. </li>
+<li>Publication r&eacute;guli&egrave;re sur les clients satisfaits, nos promotions, nos avantages.</li>
+<li>Mise en avant sur tous les r&eacute;seaux d&rsquo;une vid&eacute;o r&eacute;action : &ldquo;Wow, je viens de rep &agrave; ce questionnaire en m&ecirc;me pas 5 minutes, je vais peut-&ecirc;tre gagner 30e !&rdquo;</li>
+</ul>
+<ul>
+<li>QR code classique qui, une fois scann&eacute;, am&egrave;nera vers notre landing page. </li>
+<li>QR code complexe, qui une fois scann&eacute;, permettra d&rsquo;avoir divers avantages (r&eacute;duction, des prix, etc.)</li>
+</ul>
+<ol start="2">
+<li>
+<h2>Contenu de la landingpage</h2>
+</li>
+</ol>
+<p>R&eacute;alisation</p>
+<ul>
+<li>Pr&eacute;sentation du projet de seconde main</li>
+<li>Inciter le client &agrave; s&rsquo;inscrire au formulaire</li>
+</ul>
+<p>Mode op&eacute;ratoire</p>
+<ul>
+<li>Phrase d&rsquo;accroche</li>
+<li>Nombre de client d&eacute;j&agrave; inscrit</li>
+<li>Compteur avant la fin de l&rsquo;&eacute;v&eacute;nement</li>
+<li>R&eacute;action/avis des clients sur les r&eacute;seaux (instagram, twitter, etc.)</li>
+<li>Calendrier des &eacute;v&eacute;nements / annonces</li>
+<li>Ce qu&rsquo;on propose</li>
+<li>Nos partenaires =&gt; (trouver et changer les logos)<hr /></li>
+<li>R&eacute;action des professionnels</li>
+<li>Initiative humanitaire (UNICEF)</li>
+<li>FAQ</li>
+<li>Nous contacter (chatbot)</li>
+<li>Lien vers le formulaire</li>
+<li>Newsletter</li>
